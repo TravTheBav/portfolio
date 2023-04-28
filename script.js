85f11a387dd03323e204b38ed7c3303849c33a10
TravTheBav <tzito93@gmail.com>
2023-04-28 11:56:11 -0400

    light switch for light and dark modes

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
-let doorBell = document.querySelector('button.door_bell');
-let door = document.querySelector('.door');
+function setupDoorBell() {
+    let container = document.querySelector('.container');
+    let doorBell = document.querySelector('button.door_bell');
+    let door = document.querySelector('.door');
+    let statusBar = document.querySelector('.status_bar');
+    
+    doorBell.addEventListener('click', function () {
+        door.classList.add('open');
+        statusBar.classList.add('open');
+        setTimeout(() => { document.body.removeChild(container) }, 1600);
+    })
+};
 
-doorBell.addEventListener('click', function () {
-    door.classList.add('open');
-})
+function setupLightSwitch() {
+    let lightSwitch = document.querySelector('.light_switch img');
+    let body = document.body;
+
+    lightSwitch.addEventListener('click', function () {
+        if (lightSwitch.classList.contains('light')) {
+            lightSwitch.classList.remove('light');
+            lightSwitch.classList.add('dark');
+            body.classList.remove('light');
+            body.classList.add('dark');
+        }   else {
+            lightSwitch.classList.remove('dark');
+            lightSwitch.classList.add('light');
+            body.classList.remove('dark');
+            body.classList.add('light');
+        }
+    });
+};
+
+function initPageFunctions() {
+    setupDoorBell();
+    setupLightSwitch();
+}
+
+initPageFunctions();
